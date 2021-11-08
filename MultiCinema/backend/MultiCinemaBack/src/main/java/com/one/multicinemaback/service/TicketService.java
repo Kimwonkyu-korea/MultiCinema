43e63b914d8dc913fe9b483def0e0cfcac1d19fe
wonkyu <dnjsrb7617@naver.com>
2021-11-08 10:15:06 +0900

    final

---
+++ MultiCinema/backend/MultiCinemaBack/src/main/java/com/one/multicinemaback/service/TicketService.java
@@ -0,0 +1,22 @@
+package com.one.multicinemaback.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.one.multicinemaback.dao.TicketDao;
+import com.one.multicinemaback.dto.TicketDto;
+
+@Service
+@Transactional
+public class TicketService {
+	
+	@Autowired
+	TicketDao dao;
+	
+	public List<TicketDto> mypage(String id){
+		return dao.mypage(id);
+	}
+}
